38a4a49662781aae95eac1c460a5ab9860dfe341
Desperado <113103241+desperadoNikita@users.noreply.github.com>
2023-03-20 07:36:09 +0200

    Add files via upload

---
+++ DOM.js
@@ -0,0 +1,17 @@
+const bodyElement = document.body;
+
+const someChild = bodyElement.firstElementChild;
+console.log(someChild);
+    // 
+    // 
+ const divElement = document.querySelector("div");
+ console.log(divElement);
+    // 
+    // 
+    const divElement1 = document.querySelector(".block");
+    console.log(divElement1);
+    // 
+    // 
+    const divElement2 = document.getElementById("2");
+    console.log(divElement2);
+ 
